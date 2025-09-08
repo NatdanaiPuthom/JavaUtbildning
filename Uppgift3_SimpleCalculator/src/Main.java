448fefc0c02a868461c3a95ec1b8fbd4c00d8541
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2025-09-08 17:46:58 +0200

    Uppgift3 completed

--- Uppgift3_SimpleCalculator/src/Main.java
+++ Uppgift3_SimpleCalculator/src/Main.java
@@ -1,9 +1,20 @@
-import Uppgifter.Uppgift2;
+import Uppgifter.Uppgift3;
 
 public class Main
 {
     public static void main(String[] args)
     {
+        final Uppgift3 uppgift3 = new Uppgift3();
 
+        final char operation = uppgift3.ChooseOperation();
+
+        System.out.print("Enter first value: ");
+        final double valueA = uppgift3.ReadNumericInput();
+
+        System.out.print("Enter second value: ");
+        final double valueB = uppgift3.ReadNumericInput();
+
+        final double result = uppgift3.Calculate(operation, valueA, valueB);
+        System.out.print("Result: " + result);
     }
 }
--- Uppgift3_SimpleCalculator/src/Uppgifter/Uppgift3.java
+++ Uppgift3_SimpleCalculator/src/Uppgifter/Uppgift3.java
@@ -1,6 +1,80 @@
 package Uppgifter;
 
+import java.util.Scanner;
+
 public class Uppgift3
 {
+    final private char[] myOperations = {'+', '-', '*', '/'};
 
+    public char ChooseOperation()
+    {
+        Scanner scanner = new Scanner(System.in);
+        char operation = '0';
+
+        System.out.println("Pick a operation: Addition(+), Substraction(-), Multiplication(*) or Division(/).");
+
+        while (true)
+        {
+            System.out.print("Operation: ");
+            final String input = scanner.nextLine().trim().toLowerCase();
+
+            if (input.isEmpty())
+            {
+                System.out.println("Please pick a operation before proceeding.");
+                continue;
+            }
+
+            operation = input.charAt(0);
+
+            for (char character : myOperations)
+            {
+                if (character == operation)
+                {
+                    return operation;
+                }
+            }
+
+            System.out.println("Please pick a correct operation.");
+        }
+    }
+
+    public double ReadNumericInput()
+    {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        return scanner.nextDouble();
+    }
+
+    public double Calculate(char aOperation, double aValueA, double aValueB)
+    {
+        double result = 0.0;
+
+        switch (aOperation)
+        {
+            case '+':
+                result = aValueA + aValueB;
+                break;
+            case '-':
+                result = aValueA - aValueB;
+                break;
+            case '*':
+                result = aValueA * aValueB;
+                break;
+            case '/':
+                if (aValueB == 0)
+                {
+                    result = -1.0;
+                    System.out.println("Division by zero error!");
+                }
+                else
+                {
+                    result = aValueA / aValueB;
+                }
+                break;
+            default:
+                System.out.println("Shouldn't be possible to reach here.");
+                break;
+        }
+
+        return result;
+    }
 }
