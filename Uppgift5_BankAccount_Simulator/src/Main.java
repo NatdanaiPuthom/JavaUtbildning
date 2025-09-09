6caf60fee8100e6bb11c79c2ce553f69006acf48
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2025-09-09 17:06:27 +0200

    Uppgift 5 completed

--- Uppgift5_BankAccount_Simulator/src/Main.java
+++ Uppgift5_BankAccount_Simulator/src/Main.java
@@ -5,5 +5,18 @@ public class Main
     public static void main(String[] args)
     {
         Uppgift5 uppgift5 = new Uppgift5();
+
+        uppgift5.ShowBalance();
+
+        while (true)
+        {
+            boolean status = uppgift5.Running();
+
+            if (status == false)
+            {
+                System.out.println("\nPolice will be here shorty, please wait.");
+                break;
+            }
+        }
     }
 }
--- Uppgift5_BankAccount_Simulator/src/Uppgifter/Uppgift5.java
+++ Uppgift5_BankAccount_Simulator/src/Uppgifter/Uppgift5.java
@@ -1,7 +1,71 @@
 package Uppgifter;
 
+import java.util.Scanner;
+
 public class Uppgift5
 {
-   
+    private double myCurrentBalance = 100.0;
 
+    public void ShowBalance()
+    {
+        System.out.println("\nBalance: " + myCurrentBalance);
+    }
+
+    public boolean Running()
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\nDo you want to add balance or withdraw?");
+        System.out.print("Please enter \"A\" for Add Balance or \"W\" for Withdraw: ");
+
+        String input = scanner.nextLine().trim().toLowerCase();
+
+        while (input.isEmpty()
+                || (input.charAt(0) != 'a' && input.charAt(0) != 'w')
+        )
+        {
+            System.out.print("Please enter \"A\" for Add Balance or \"W\" for Withdraw: ");
+            input = scanner.nextLine().trim().toLowerCase();
+        }
+
+        if (input.charAt(0) == 'a')
+        {
+            return AddBalance(scanner);
+        } else
+        {
+            return Withdraw(scanner);
+        }
+    }
+
+    private boolean AddBalance(Scanner aScanner)
+    {
+        System.out.print("\nHow much would you like to add?: ");
+        double amount = aScanner.nextDouble();
+
+        if (amount <= 1.0f)
+        {
+            System.out.print("Please enter minimum 1.0: ");
+            return false;
+        }
+
+        myCurrentBalance += amount;
+        System.out.println("Successfully added. New balance: " + myCurrentBalance);
+        return true;
+    }
+
+    private boolean Withdraw(Scanner aScanner)
+    {
+        System.out.print("\nHow much would you like to withdraw?: ");
+        double amount = aScanner.nextDouble();
+
+        if (myCurrentBalance < amount)
+        {
+            System.out.println("Not enough balance. Current balance: " + myCurrentBalance);
+            return false;
+        }
+
+        myCurrentBalance -= amount;
+        System.out.println("Successfully withdraw. New balance: " + myCurrentBalance);
+        return true;
+    }
 }
