c09017101f0aa9236d685793cf952976c803c550
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2025-09-09 15:01:55 +0200

    Removed old code from copy paste

--- Uppgift4_TemperatureConverter/src/Main.java
+++ Uppgift4_TemperatureConverter/src/Main.java
@@ -1,20 +1,9 @@
-import Uppgifter.Uppgift3;
+import Uppgifter.Uppgift4;
 
 public class Main
 {
     public static void main(String[] args)
     {
-        final Uppgift3 uppgift3 = new Uppgift3();
 
-        final char operation = uppgift3.ChooseOperation();
-
-        System.out.print("Enter first value: ");
-        final double valueA = uppgift3.ReadNumericInput();
-
-        System.out.print("Enter second value: ");
-        final double valueB = uppgift3.ReadNumericInput();
-
-        final double result = uppgift3.Calculate(operation, valueA, valueB);
-        System.out.print("Result: " + result);
     }
 }
