d116caa4e4885fedbb950fe077d3e60903ebef4b
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2025-09-10 10:43:27 +0200

    GruppUppgift

--- Uppgift7_StudentGradeForAClass/src/Uppgifter/Uppgift7.java
+++ Uppgift7_StudentGradeForAClass/src/Uppgifter/Uppgift7.java
@@ -1,6 +1,17 @@
 package Uppgifter;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Uppgift7
 {
+    public void Task()
+    {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("How many students?: ");
+        int studentAmount = scanner.nextInt();
+
+        ArrayList<Double> students = new ArrayList<Double>(studentAmount);
+    }
 }
